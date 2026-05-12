4a3e077ebd2526f2a4b4f7b3a2a26ceb6fc39d71
Ryan Shill <ryan@ryanshill.com>
2026-05-12 17:36:59 -0500

    fixes

--- script.js
+++ script.js
@@ -50,40 +50,45 @@ window.onload = function() {
 }
 
 //Hamburger Menu JS
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navBarLinks = document.getElementsByClassName('navlinks')[0]
-const title = document.getElementsByClassName('title')[0]
+//Hamburger Menu JS
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
 
+if (toggleButton) {
+    const navBarLinks = document.getElementsByClassName('navlinks')[0];
+    const title = document.getElementsByClassName('title')[0];
 
-toggleButton.addEventListener('click', () => {
-    navBarLinks.classList.toggle('active')
-    title.classList.toggle('active')
-})
+    toggleButton.addEventListener('click', () => {
+        navBarLinks.classList.toggle('active');
+        title.classList.toggle('active');
+    });
+}
 
 //Contact Form JS
 
 const contactForm = document.getElementById('contact');
-const banner = document.getElementsByClassName('formsubmittedbanner')[0];
 
-contactForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  
-  const formData = new FormData(contactForm);
-  
-  const response = await fetch('https://www.ryanshill.com/api/contact', {
-    method: 'POST',
-    body: new URLSearchParams(formData)
-  });
+if (contactForm) {
+    const banner = document.getElementsByClassName('formsubmittedbanner')[0];
+    const close = document.getElementsByClassName('closebtn')[0];
 
-  if (response.ok) {
-    banner.classList.toggle('active')
-  }
-});
+    contactForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
 
-const close = document.getElementsByClassName('closebtn')[0];
+        const formData = new FormData(contactForm);
 
-close.addEventListener('click', () => {
-    banner.classList.toggle('active')
-})
+        const response = await fetch('https://www.ryanshill.com/api/contact', {
+            method: 'POST',
+            body: new URLSearchParams(formData)
+        });
+
+        if (response.ok) {
+            banner.classList.toggle('active');
+        }
+    });
+
+    close.addEventListener('click', () => {
+        banner.classList.toggle('active');
+    });
+}
 
 
